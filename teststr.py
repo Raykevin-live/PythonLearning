a16de47bab4badcb5dfea9d48396d57bc2a42c86
RayKevin-live <RayKevin121@gmail.com>
2024-10-23 23:36:22 +0800

    字符串类型

---
+++ teststr.py
@@ -0,0 +1,15 @@
+# s = ['www','cau','edu']
+# url = '.'.join(s)
+
+# print(url)
+
+# s = '    Hello World   '
+# s1 = s.rstrip()
+# print(s1)
+
+# a = input()
+# print(type(a))
+
+import calendar
+
+print(calendar.month(2024, 10))
